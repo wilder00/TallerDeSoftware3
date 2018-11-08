0940ad3d9f8e462fe5f3e404771c68c1c7b22fd8
Wilder Trujillo <nadie437@gmail.com>
2018-11-07 21:42:23 -0500

    la clase tienda sin terminar los métodos

--- src/mis/clases/Tienda.java
+++ src/mis/clases/Tienda.java
@@ -15,24 +15,36 @@ public class Tienda {
     
     private int codSucursal;
     private String distrito;
+    
     private CajaDePago []cajas;
+    private final int TAM_CAJAS = 10;
     private int ncajas;
 
     //Constructor con parámetro (código de sucursal, distrito) aquí el arreglo se crea vacío.
     public Tienda(int codSucursal, String distrito) {
         this.codSucursal = codSucursal;
         this.distrito = distrito;
-        this.cajas = new CajaDePago[10];
+        this.cajas = new CajaDePago[TAM_CAJAS];
         this.ncajas = 0;
     }
     
     // Método que seleccione aleatoriamente una caja e inserte un nuevo cliente a la cola,
     // en caso la caja no este habilitado debe crearla para insertar al cliente en la cola.
     
-    public void insertarclienteACaja(){
+    public void insertarclienteACaja(Cliente c){
         Random ran = new Random();
+        int num = ran.nextInt(TAM_CAJAS);
+        //Quiero validar el caso en que la caja este llena de clientes, y el caso en que todas las cajas esten llenas
+        while(num != -1){
+            if(cajas[num]==null){
+                CajaDePago nuevaCaja = new CajaDePago();
+                cajas[num]= nuevaCaja;
+            }
+            
+            if(!cajas[num].getClientes().colaLlena())
+            cajas[num].agregarCliente(c);
+        }
         
-    }
     
     
     
